eccf7868b7222ff99c6968d3e124217fd5859a5c
James Rimmer <jr49@st-andrews.ac.uk>
2020-04-21 16:46:19 +0100

    Verify commit

--- Simple linear simulations.R
+++ Simple linear simulations.R
@@ -3,13 +3,13 @@ a <- rnorm(1e4, mean = 6, sd = 20) # Generate some random numbers according to a
 plot(a) # Show a normal distribution
 plot(density(a))
 
-##### Simple plots first
+##### Simple distribution plot first using 'real' values
 set.seed(20)
 chlorophyll <- rnorm(60, mean = 7, sd = 2) # Simulating some chlorophyll values; SD and mean come from
 # Assemble experiment
 plot(density(chlorophyll))
 
-##### Now including predictors
+##### Now including an error term
 set.seed(20)
 x <-rnorm(60, mean = 7, sd = 2) # Data generating process
 e <- rnorm(6, sd = 2) # Error
@@ -19,4 +19,67 @@ plot(x, chlor) # Plot these data
 m1 <- lm(chlor ~ x) # Model of the relationship between x and chlorophyll
 summary(m1) # Can see the model predicts they are strongly related, with an estimate of nearly 1. 
 abline(m1$coef[1], m1$coef[2], col = "red")
+
+#####
+set.seed(20)
+x <-rnorm(60, mean = 7, sd = 2) # Data generating process
+e <- rnorm(6, sd = 2) # Error
+chlor <- x + e # Add the error and the data generating process
+summary(chlor)
+plot(x, chlor) # Plot these data
+m1 <- lm(chlor ~ x) # Model of the relationship between x and chlorophyll
+summary(m1) # Can see the model predicts they are strongly related, with an estimate of nearly 1. 
+abline(m1$coef[1], m1$coef[2], col = "red")
+
+##### Next, need simulate with different groups
+set.seed(16)
+ngroup <- 2
+nrep <- 10
+b0 <- 5
+b1 <- -2
+sd <- 2
+group <- rep( c("group1", "group2"), each = nrep) # 10 group 1s, 10 group 2s
+eps <- rnorm(ngroup*nrep, mean = 0, sd = sd) # Error term for each observation
+growth <- b0 + b1*(group == "group2") + eps # Create the simulated response; growth is now given by a true
+# group mean for group1 (b0), a mean for group2 which is 2 less than group1 (b1 = -2; -2*group 2 in 
+# formula), and an error epsilon (eps).
+
+growthfit <- lm(growth ~ group)
+summary(growthfit)
+
+# This is one model, but we want to know how it functions over the long run. So let's create (borrow) a 
+# function to do this:
+
+twogroup_fun <- function(nrep = 10, b0 = 5, b1 = -2, sigma = 2){
+  ngroup = 2
+  group = rep(c("group1", "group2"), each = nrep)
+  eps = rnorm(ngroup*nrep, 0, sigma)
+  growth = b0 + b1*(group=="group2") + eps
+  growthfit = lm(growth ~ group)
+  growthfit
+}
+
+# This function is using the same parameters as above. 
+
+# Now test the function
+
+set.seed(16)
+twogroup_fun()
+twogroup_fun(sigma=1) # Example of how to change an element of the function
+
+### Now repeat the function
+
+sims <- replicate(1e3, twogroup_fun(), simplify = F)
+
+
+
+
+
+
+
+
+
+
+
+
 
