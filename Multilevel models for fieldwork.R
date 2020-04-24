5f92c2bed8b94ab95483454e88ce492c73a625bb
James Rimmer <jr49@st-andrews.ac.uk>
2020-04-24 12:13:59 +0100

    Building the factor and covariate multilevel model

--- Multilevel models for fieldwork.R
+++ Multilevel models for fieldwork.R
@@ -1,6 +1,6 @@
-          ############################################################
-          ####    Mixed effects model simulation with 2 levels    ####
-          ############################################################
+             ############################################################
+             ####    Mixed effects model simulation with 2 levels    ####
+             ############################################################
 library(lme4)
 # In this simulation, we take a single estuarine system and sample from it in 5 patches. In each patch, we
 # conduct measurements of chlorophyll, nutrients and grazer biomass
@@ -37,3 +37,42 @@ summary(cmm1)
 # In this simulation, we take a single estuarine system and sample from it in 5 patches. In each patch, we
 # conduct an experiment - glyphosate at 5 levels - and replicate it twice. So 10 observations per patch.
 # This first example has one fixed effect - this 5 level factor
+
+
+# Could we build in other sources of noise, such as salinity/rain etc, and use them in the data generating 
+# process but not account for them as fixed effects?
+
+
+                    ######################################################
+                    #####       Fitting a categorical variable        ####
+                    ######################################################
+
+set.seed(20)
+npatch <- 5
+nexp <- 6
+b0 <- 3 # Mean when other variables at zero or level 1 factor (control)
+b1 <- -0.3 # Mean change in response for 1 unit change in nutrients
+b2 <- -0.05# Difference between control and level 1
+b3 <- -0.1 # Difference between control and level 2
+b4 <- -0.25 # Difference between control and level 3
+b5 <- -2 # Difference between control and level 4
+
+gly <- rep(c("Control", "lvl1", "lvl2", "lvl3", "lvl4"),  nexp)
+
+sds <- 2
+sd <- 1.5
+
+patch <- rep(LETTERS[1:npatch], each = nexp) # Name the patches;  experiment within a patch share a name
+exp <- as.factor(1:(npatch*nexp)) # Names for each experiment (all unique)
+
+patcheff <- rep(rnorm(npatch, 0, sds), each = npatch) # Patch level variation
+expeff <- rnorm(npatch*nexp, 0, sd) # Observation level variation
+
+
+
+
+
+
+
+
+
