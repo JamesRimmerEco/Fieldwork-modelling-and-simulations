bdb4e6c42612db05fc6b089ee382e91ffbe5ef9b
James Rimmer <jr49@st-andrews.ac.uk>
2020-04-28 17:00:32 +0100

    Test statistical power of model

--- Temporal simulations.R
+++ Temporal simulations.R
@@ -53,6 +53,11 @@ plot_model(m1x, type = "pred", terms = c("time", "obs[1:9]"), pred.type = "re") 
 plot_model(m1x, type = "pred", terms = c("stress", "obs[1:9]"), pred.type = "re") 
 # And we see here that the global stress slope is 4.92...(etc) from the fixed effects
 
+# Can also pull parameters out like this:
+tidy(m1x, effects = "fixed")
+tidy(m1x, effects = "ran_pars", scales = "vcov")
+
+
 # Let's extract run multiple simulations and extract some parameters
 library(broom)
 library(dplyr)
@@ -100,4 +105,67 @@ sims %>%
   geom_density(fill = "blue", alpha = .5) + 
   geom_vline( xintercept = 1.5)
 
+#  But what about the effect of sample size?
+# Let's try a few sizes:
 
+stresstempo_fun90 <- function(nexp = 90, ntime = 5, b0 = 50, b1 = 5, bt = 1.5){
+  stress <- rnorm(nexp, 0, 3)
+  time <- rep((1:ntime), each = nexp/ntime)
+  sd <- 2
+  obs <- rep(as.factor(1:(nexp/ntime)), ntime)
+  obseff <- rnorm(nexp, 0, sd)
+  df <- data.frame(stress, time, obs, obseff)
+  df
+  resp <- b0 + b1*stress + bt*time + obseff
+  df <- data.frame(df, resp)
+  df
+  m1 <- lm(resp ~ stress + time, data = df)
+  m1
+}
+
+sims90 <- replicate(1e3, stresstempo_fun90(), simplify = F)
+sims90 %>%
+  map_df(tidy) %>%
+  filter(term == "stress") %>%
+  ggplot( aes(estimate) ) +
+  geom_density(fill = "blue", alpha = .5) + 
+  geom_vline( xintercept = 5)
+
+stresstempo_fun450 <- function(nexp = 450, ntime = 5, b0 = 50, b1 = 5, bt = 1.5){
+  stress <- rnorm(nexp, 0, 3)
+  time <- rep((1:ntime), each = nexp/ntime)
+  sd <- 2
+  obs <- rep(as.factor(1:(nexp/ntime)), ntime)
+  obseff <- rnorm(nexp, 0, sd)
+  df <- data.frame(stress, time, obs, obseff)
+  df
+  resp <- b0 + b1*stress + bt*time + obseff
+  df <- data.frame(df, resp)
+  df
+  m1 <- lm(resp ~ stress + time, data = df)
+  m1
+}
+
+sims450 <- replicate(1e3, stresstempo_fun450(), simplify = F)
+sims450 %>%
+  map_df(tidy) %>%
+  filter(term == "stress") %>%
+  ggplot( aes(estimate) ) +
+  geom_density(fill = "blue", alpha = .5) + 
+  geom_vline( xintercept = 5)
+
+# Sample size increasing does help with the estimation
+
+# Given these simulations, how often do we detect an effect?
+
+sims %>%
+  map_df(tidy) %>%
+  filter(term == "stress") %>%
+  pull(p.value) %>%
+  {. <  0.05} %>%
+  mean()
+
+# This code is putting the parameters into a table from the model, filtering for stress, extracting the 
+# p value for each model, and finding out what proportion is below an alpha of 0.05. In this case,
+# 100% of the models correctly detect a significant effect (i.e. reject a null of no effect of stress on
+# the response. 
